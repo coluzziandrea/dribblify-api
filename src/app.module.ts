f9c52686991a1fe6392368568444ea29f142ea0b
Coluzzi Andrea <coluzziandrea.dev@gmail.com>
2023-11-23 17:16:57 +0000

    feat: installed ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'simform',
+      username: 'postgres',
+      entities: [],
+      database: 'pgWithNest',
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
